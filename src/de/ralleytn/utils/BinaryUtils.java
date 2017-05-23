56be50587f6773113f9674a8dbab83c6042c476f
Ralph Niemitz <ralph.niemitz@gmx.de>
2017-05-23 20:40:00 +0200

    Fixed a heavy bug
    Signed-off-by: Ralph Niemitz <ralph.niemitz@gmx.de>

--- src/de/ralleytn/utils/BinaryUtils.java
+++ src/de/ralleytn/utils/BinaryUtils.java
@@ -63,7 +63,7 @@ public final class BinaryUtils {
 	 */
 	public static final int getSignedInteger(int o1, int o2, int o3, int o4) {
 		
-		return ((o1 & 0xFF) << 24) | ((o2 & 0xFF) << 16) | ((o3 & 0xFF) << 8) | (o1 & 0xFF);
+		return ((o1 & 0xFF) << 24) | ((o2 & 0xFF) << 16) | ((o3 & 0xFF) << 8) | (o4 & 0xFF);
 	}
 	
 	/**
